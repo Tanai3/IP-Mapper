cddfd81d2b0114d9efe0895fbc6024ff8ce2a833
chayka1206 <ec.chayka@gmail.com>
2017-07-14 00:54:48 +0900

    upload

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ apt-get install python3-pyqt4
 apt-get install libpcap-dev
 pip install geoip2
 pip install pcapy
+
+wget geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz
+mkdir -v GeoIP
+mv -v GeoLite2-City.mmdb.gz ./GeoIP
+gunzip -v GeoIP/GeoLite2-City.mmdb.gz
+
